b61c96aef8e7229d0322cbc630cfa8ec209a2595
EsKill01 <90495513+EsKill01@users.noreply.github.com>
2022-05-02 21:18:28 -0600

    Add files via upload

---
+++ Dependicy inversion/StartApp/Ejercicios/ConsoleUI/Factory.cs
@@ -0,0 +1,20 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class Factory
+    {
+        public static IPerson CreatePerson() => new Person();
+
+        public static IChore CreateChore() => new Chore(CreateLogger(), CreateMessageSender());
+
+        public static ILogger CreateLogger() => new Logger();
+
+        public static IMessageSender CreateMessageSender() => new Texter();
+    }
+}
